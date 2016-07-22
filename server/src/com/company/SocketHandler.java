ce4aeaff78978aa1cfc43a71c9693c34de903ab6
Kazin <xkazinx@gmail.com>
2016-07-22 00:25:57 -0300

    Added SocketHandler debug prints

--- server/src/com/company/SocketHandler.java
+++ server/src/com/company/SocketHandler.java
@@ -6,31 +6,43 @@ import java.net.Socket;
 import java.net.InetAddress;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class SocketHandler {
     public SocketHandler(String ip, int port) throws IOException
     {
         _listener = new ServerSocket(port, 256, InetAddress.getByName(ip));
+        System.out.printf("Socket opened at %s:%d\n", ip, port);
     }
 
     private ServerSocket _listener;
+    public ArrayList<Socket> _clients;
 
     public void Tick() throws IOException
     {
         try {
             while (true) {
-                Socket socket = _listener.accept();
-                try {
-                    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                System.out.println("New Client accepted");
+                Socket peer = _listener.accept();
+                _clients.add(peer);
+                try
+                {
+                    PrintWriter out = new PrintWriter(peer.getOutputStream(), true);
                     out.println(new Date().toString());
-                } finally {
-                    socket.close();
+                    System.out.println("Sent date to client");
+                }
+                finally
+                {
+                    peer.close();
+                    _clients.remove(peer);
+                    System.out.println("Client removed");
                 }
             }
         }
         finally {
             _listener.close();
+            System.out.println("Socket Closed");
         }
     }
 
