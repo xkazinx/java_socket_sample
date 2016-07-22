977844874650c9053f144d486069bc148092cb22
GaBPC <gabprietoyt@gmail.com>
2016-07-22 00:31:04 -0300

    Client task added

--- client/src/com/company/Main.java
+++ client/src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        try {
+            String serverAddress = "127.0.0.1";
+            int port = 9090;
+            ClientTask ct = new ClientTask(serverAddress,port);
+            System.out.println(ct.GetDate());
+            ct.EndConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
